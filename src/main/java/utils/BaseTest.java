a6f46f89019448b3d9c59cb5e7fa9c9966108f6b
Adrian Romulus Vlaicu <adivlaicu72@gmail.com>
2023-09-01 18:10:06 +0300

    curs26

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -5,14 +5,17 @@ import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 	
 	public static WebDriver driver;
 	public BasePage app;
 	
-	@BeforeClass(alwaysRun = true)
+//	@BeforeClass(alwaysRun = true)
+	@BeforeMethod(alwaysRun = true)
 	public void setup() {
 		//System.setProperty("webdriver.chrome.driver", "path catre/chromedriver.exe")
 		driver = new ChromeDriver();
@@ -23,7 +26,8 @@ public class BaseTest {
 		app = new BasePage();
 	}
 	
-	@AfterClass
+//	@AfterClass
+	@AfterMethod
 	public void teardown() throws InterruptedException {
 		Thread.sleep(4000);//bad practice
 		
